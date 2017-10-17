cdc2a0ae574f169849325a2c469cba80b6a18cf7
Dustin Dowell <dustindowell22@gmail.com>
2017-10-17 13:49:41 -0500

    rename queries to query

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ module.exports = {
     }
     return result
   },
-  encode: function (queries) {
+  encode: function (query) {
     var result = '?'
-    for (var key in queries) {
-      if (queries[key] != null) {
-        result += key + '=' + queries[key] + '&'
+    for (var key in query) {
+      if (query[key] != null) {
+        result += key + '=' + query[key] + '&'
       }
     }
     return result.slice(0, -1)

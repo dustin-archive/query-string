a9a00807ba66d7f22eea7404777ac43ebd005dea
Dustin Dowell <dustindowell22@gmail.com>
2017-10-30 17:39:23 -0500

    convert to es2015 imports

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-module.exports = {
-  decode: function (query) {
-    query = query.slice(1).split('&')
-    var result = {}
-    for (var i = 0, pair; i < query.length; i++) {
-      pair = query[i].split('=')
-      result[pair[0]] = pair[1]
-    }
-    return result
-  },
-  encode: function (query) {
-    var result = '?'
-    for (var key in query) {
-      if (query[key] != null) {
-        result += key + '=' + query[key] + '&'
-      }
-    }
-    return result.slice(0, -1)
+function decode (query) {
+  query = query.slice(1).split('&')
+  var result = {}
+  for (var i = 0, pair; i < query.length; i++) {
+    pair = query[i].split('=')
+    result[pair[0]] = pair[1]
   }
+  return result
 }
+
+function encode (query) {
+  var result = '?'
+  for (var key in query) {
+    if (query[key] != null) {
+      result += key + '=' + query[key] + '&'
+    }
+  }
+  return result.slice(0, -1)
+}
+
+export { decode, encode }
